ed94b698e236ab1d53baa14e952ae0eaa8c9a5fe
alumnos <robertogonzalesayala@gmail.com>
2026-09-07 11:55:50 -0500

    refactor: update function names for UUID generation and validation

--- src/shared/domain/model/uuid.js
+++ src/shared/domain/model/uuid.js
@@ -1,9 +1,9 @@
 import { v7 as uuid7, validate as uuidValidate } from 'uuid';
 
-export function generateUUID() {
+export function generateUuid() {
     return uuid7();
 }
 
-export function isValidUuid(uuid) {
+export function validateUuid(uuid) {
     return uuidValidate(uuid);
 }
